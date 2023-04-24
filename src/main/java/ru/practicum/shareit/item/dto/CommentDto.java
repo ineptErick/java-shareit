6da4d90299a2cdc8d44ce4ce570ea12b1c61b2fa
ineptErick <inepterick@gmail.com>
2023-04-25 00:04:13 +0300

    add_bookings bug fixed

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,12 +1,12 @@
 package ru.practicum.shareit.item.dto;
 
-
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class CommentDto {
+
     private Long id;
 
     private String text;
